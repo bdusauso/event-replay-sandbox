3ae6b239a27a7e280d59decd3223634db2e0cc3a
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2017-12-06 11:21:44 +0100

    Trim down code to its minimum

--- lib/replay.rb
+++ lib/replay.rb
@@ -4,10 +4,6 @@ require "thread"
 
 class Replay
 
-  REPLAY_EXCHANGE = "replay".freeze
-  MAX_RETRIES     = 5
-  PREFETCH        = ENV.fetch("PLUMBING_EVENT_REPLAY_PREFETCH", 250).to_i
-
   SUBSCRIBE_OPTIONS = {
     manual_ack: true,
     exclusive:  true
@@ -18,13 +14,10 @@ class Replay
   end
 
   def run
-    conn = Bunny.new.start
-
-    chan = conn.create_channel
-    chan.prefetch(PREFETCH)
-
+    conn  = Bunny.new.start
+    chan  = conn.create_channel
     queue = chan.queue(@queue_name)
-    exch  = chan.direct(REPLAY_EXCHANGE, durable: true)
+    exch  = chan.direct("replay", durable: true)
 
     queue.bind(exch, routing_key: @queue_name)
 
@@ -34,19 +27,18 @@ class Replay
     conn.close
   end
 
+  private
 
   def replay_events_loop(channel, queue)
-    semaphore   = Mutex.new
     replay_done = false
 
-    queue.subscribe(SUBSCRIBE_OPTIONS) do |delivery_info, _, payload|
+    consumer = queue.subscribe(SUBSCRIBE_OPTIONS) do |delivery_info, _, payload|
       begin
         next_loop_state = process_payload(JSON.parse(payload))
 
         channel.ack(delivery_info.delivery_tag)
         if next_loop_state == :stop
-          channel.consumers[delivery_info.consumer_tag].cancel
-          semaphore.synchronize { replay_done = true }
+          replay_done = true
         end
       rescue => exception
         channel.reject(delivery_info.delivery_tag, true)
@@ -56,37 +48,18 @@ class Replay
 
     # Run forever until next_loop_state == :stop.
     loop do
-      sleep 0.5
-      semaphore.synchronize do
-        puts "Replay done? #{replay_done}"
+      sleep 2
         if replay_done
+          consumer.cancel
           puts "Finished replaying events"
           return
-        end
       end
     end
 
   end
 
   def process_payload(payload)
-    command_name = payload.fetch("meta", {}).fetch("command", nil)
-
-    if command_name
-      process_command(command_name, payload.fetch("data"))
-    else
-      process_event(payload)
-    end
-  end
-
-  def process_command(name, data)
-    case name
-      when "replay_complete"
-        :stop
-    end
-  end
-
-  def process_event(payload)
-    :continue
+    payload.fetch("meta", {}).fetch("command", nil) ? :stop : :continue
   end
 
 end
